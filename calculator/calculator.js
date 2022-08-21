86c349cda452e8e5b9ac59711b7eb4b18cccffae
Krishna Sen Program <98060735+Krishna-Sen-Programming-World@users.noreply.github.com>
2022-08-21 06:43:17 +0530

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -13,7 +13,7 @@ function fn1(){
         {ans+=parseFloat(sr[i+1]);}
         if(sr[i]=='-')
         {ans-=parseFloat(sr[i+1]);}
-        if(sr[i]=='×')
+        if(sr[i]=='*')
         {ans*=parseFloat(sr[i+1]);}
         if(sr[i]=='%')
         {ans%=parseFloat(sr[i+1]);}
@@ -29,14 +29,17 @@ function fn1(){
 function fner(){
     var wor=document.getElementById('in').value;
     const rm= wor.length-1;
-    if(wor[rm]==' ')
-    document.getElementById('in').value= wor.substring(0,rm-2);
-    else
-    document.getElementById('in').value= wor.substring(0,rm);
+    if(wor[rm]==' '){
+    document.getElementById('in').value= wor.substring(0,rm-2);}
+    else{
+    document.getElementById('in').value= wor.substring(0,rm);}
+
 }
-function ce(){
+
+
+function clr(){
     var wor=[];
-    document.getElementById('in').value= wor;
+    document.getElementById('in').value=wor;
 }
 function fcp(){
     var fc=document.getElementById('demo').innerHTML;
@@ -45,5 +48,20 @@ function fcp(){
 function fpt(){
     var wo=document.getElementById('demoo').innerHTML;
     document.getElementById('in').value+=wo;
-
+}
+function funent()
+{
+    var k=document.getElementById('doneeok');
+    k.addEventListener('keypress',function(ent){
+        if(ent.key=='='){
+            document.getElementById('doneok').click();
+        }
+        else if(ent.key>=0 && ent.key<=9)
+        {
+            fn(ent.key);
+        }
+        else if( ent.key == '+' || ent.key === '-'|| ent.key == '*' || ent.key == '%' || ent.key == '/' || ent.key == '^'){
+            fns(ent.key);
+        }
+    })
 }
